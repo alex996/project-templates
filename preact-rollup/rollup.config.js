7ced8f8c2d26ec7622d65ce321bd6b8dd84de5e0
Alex <alexnezhynsky@gmail.com>
2018-12-18 21:33:54 -0500

    preact-rollup: Replace reload with livereload server

--- preact-rollup/rollup.config.js
+++ preact-rollup/rollup.config.js
@@ -3,28 +3,36 @@ import babel from 'rollup-plugin-babel'
 import postcss from 'rollup-plugin-postcss'
 import { terser } from 'rollup-plugin-terser'
 import resolve from 'rollup-plugin-node-resolve'
+import livereload from 'rollup-plugin-livereload'
+import serve from 'rollup-plugin-serve'
 
-const dir = 'dist'
+const dist = 'dist'
 const production = !process.env.ROLLUP_WATCH
 
 export default {
   input: 'src/index',
   output: {
-    dir,
-    file: 'bundle.js',
+    file: `${dist}/bundle.js`,
     format: 'iife',
     sourcemap: true
   },
   plugins: [
     resolve(),
     copy({
-      'index.html': `${dir}/index.html`
+      'index.html': `${dist}/index.html`
+    }),
+    livereload(),
+    serve({
+      open: true,
+      port: 3000,
+      contentBase: dist,
+      historyApiFallback: true
     }),
     babel({
       exclude: 'node_modules/**'
     }),
     postcss({
-      extract: `${dir}/styles.css`,
+      extract: `${dist}/styles.css`,
       minimize: production
     }),
     production && terser()
--- preact-rollup/src/index.js
+++ preact-rollup/src/index.js
@@ -2,6 +2,4 @@ import './style.css'
 import { h, render } from 'preact' // eslint-disable-line no-unused-vars
 import App from './App'
 
-const root = document.getElementById('root')
-
-render(<App />, root)
+render(<App />, document.getElementById('root'))
