96c4f160dbdb84fa4e969448bbf525aa3ff1dabb
Alex N <alexnezhynsky@gmail.com>
2020-07-24 10:23:45 -0600

    Update eslint config for 2020

--- node-typescript/src/index.ts
+++ node-typescript/src/index.ts
@@ -6,13 +6,13 @@ const { PORT = 3000 } = process.env
 const app = express()
 
 app.use((req: Request, res: Response, next: NextFunction): void => {
-  console.log(`${req.method} ${req.url} ${req.protocol} ${req.ip}`)
+  console.info(`${req.method} ${req.url} ${req.protocol} ${req.ip}`)
 
   next()
 })
 
 app.get('/', (req: Request, res: Response): void => {
-  res.json({})
+  res.json({ message: 'Works' })
 })
 
 app.listen(PORT, (): void => console.log(`http://localhost:${PORT}`))
